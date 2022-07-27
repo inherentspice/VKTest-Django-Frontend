adb958eb9b3a1027c35aa495932d912a4a896d3b
Stephen Smith <stephenlehanesmith@gmail.com>
2022-07-27 06:40:03 -0700

    added instructions page

--- core/views.py
+++ core/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index_view(request):
     return render(request, 'home.html')
+
+def instructions(request):
+    return render(request, 'instructions.html')
