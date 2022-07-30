71cb49f93bdce52d2bbccb53df46357acb6e1ab8
Stephen Smith <stephenlehanesmith@gmail.com>
2022-07-29 23:09:06 -0700

    modified allowed_host variable

--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*.herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 # Application definition
 
