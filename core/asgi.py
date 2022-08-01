05e91aadec754674b54f4e75f45c39f6e60a6bb9
Stephen Smith <stephenlehanesmith@gmail.com>
2022-08-01 04:29:27 -0700

    changed application

--- core/asgi.py
+++ core/asgi.py
@@ -4,9 +4,9 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 import django
 django.setup()
 
-# from django.core.asgi import get_asgi_application
+from django.core.asgi import get_asgi_application
 
-from channels.routing import ProtocolTypeRouter, URLRouter, get_default_application
+from channels.routing import ProtocolTypeRouter, URLRouter
 import chat.routing
 
 # application = get_default_application()
