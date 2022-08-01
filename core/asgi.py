10f1a96d77114a130707bafe2ec6503a450e7827
Stephen Smith <stephenlehanesmith@gmail.com>
2022-07-31 22:21:11 -0700

    added line to setup django

--- core/asgi.py
+++ core/asgi.py
@@ -2,8 +2,10 @@ from django.core.asgi import get_asgi_application
 import os
 from channels.routing import ProtocolTypeRouter, URLRouter
 import chat.routing
+import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+django.setup()
 
 application = ProtocolTypeRouter({
   'http': get_asgi_application(),
