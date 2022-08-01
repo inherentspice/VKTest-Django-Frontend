d0e7d07c3e9e863d6673befd038bd5c552fc6672
Stephen Smith <stephenlehanesmith@gmail.com>
2022-07-31 23:48:03 -0700

    added daphne to requirements

--- core/settings.py
+++ core/settings.py
@@ -39,9 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'chat.apps.ChatConfig',
+    'chat.apps.ChatConfig',
     'channels',
-    'chat',
 ]
 
 MIDDLEWARE = [
