52e3daa9672e3fd3a37c85364f1ce5e58e246abe
Stephen Smith <stephenlehanesmith@gmail.com>
2022-08-01 04:53:49 -0700

    changed format of staticfiles_dirs

--- core/settings.py
+++ core/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = 'chat/static'
+STATICFILES_DIRS = ['chat/static',]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
