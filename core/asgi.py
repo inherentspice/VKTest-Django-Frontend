5bf4fdc0d0bf464a9e4975e500b8cf70170f92dc
Stephen Smith <stephenlehanesmith@gmail.com>
2022-07-31 22:14:01 -0700

    changed order of imports

--- core/asgi.py
+++ core/asgi.py
@@ -1,8 +1,6 @@
-import os
-
-from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-
+import os
+from channels.routing import ProtocolTypeRouter, URLRouter
 import chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
