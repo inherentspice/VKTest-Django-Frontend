a192b6dd6f68a30b6fbea64734ad86fd01eefff0
Stephen Smith <stephenlehanesmith@gmail.com>
2022-07-31 22:34:33 -0700

    changed installed app name

--- core/settings.py
+++ core/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'chat.apps.ChatConfig',
+    # 'chat.apps.ChatConfig',
     'channels',
     'chat',
 ]
