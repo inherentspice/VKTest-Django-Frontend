347ebcb92ede57e2396bc1e56b99628d75458bbc
Stephen Smith <stephenlehanesmith@gmail.com>
2022-08-02 19:21:12 -0700

    adding name change

--- chat/urls.py
+++ chat/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index_view, name='chat-index'),
+    path('', views.index_view, name='chat'),
     path('<str:room_name>/<str:user_name>', views.room_view, name='chat-room'),
 ]
