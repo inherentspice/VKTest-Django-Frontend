1e560c5ec2479162fd6abea3125d67bb0f0c8e4a
Stephen Smith <stephenlehanesmith@gmail.com>
2022-08-03 02:31:33 -0700

    edited regex in routing

--- chat/routing.py
+++ chat/routing.py
@@ -1,9 +1,8 @@
-# from django.urls import re_path
-from django.conf.urls import url
+from django.urls import re_path
 
 
 from . import consumers
 
 websocket_urlpatterns = [
-    url(r'wss/chat/(?P<room_name>[A-Za-z0-9_-]+)/(?P<user_name>\[A-Za-z0-9_-]+)/(?P<role>\[A-Za-z0-9_-]+)/$', consumers.ChatConsumer.as_asgi()),
+    re_path(r'wss/chat/(?P<room_name>[A-Za-z0-9_-]+)/(?P<user_name>\[A-Za-z0-9_-]+)/$', consumers.ChatConsumer.as_asgi()),
 ]
