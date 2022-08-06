8e1eb829a7621b0528c26256d0beaa714a5ef1ba
Stephen Smith <stephenlehanesmith@gmail.com>
2022-08-05 20:53:38 -0700

    updated csrf trusted origins

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1:8000']
 
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'chat.apps.ChatConfig',
     'channels',
 ]
+
+CSRF_TRUSTED_ORIGINS=['https://*.herokuapp.com']
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
