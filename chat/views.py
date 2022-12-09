ba5daf8a15ce19dd1a8697443d2e7ce7897a1242
Stephen Smith <stephenlehanesmith@gmail.com>
2022-12-09 14:01:58 +0800

    added error handling in the views for room creation and changing the question

--- chat/views.py
+++ chat/views.py
@@ -2,6 +2,19 @@ from unicodedata import name
 from django.shortcuts import render
 import requests
 from chat.models import Room, User
+import random
+
+# List of subjective questions with specific examples
+questions = ["What do you think about the current state of politics?",
+             "How do you feel about the pandemic?",
+             "What was your experience with traveling to a new country like?",
+             "What is your opinion on climate change?",
+             "How do you handle stress?",
+             "What do you believe about the existence of extraterrestrial life?",
+             "What are your thoughts on social media?",
+             "What is your perspective on the education system?",
+             "What is your stance on gun control?",
+             "What is your view on the importance of arts and culture?"]
 
 
 def index_view(request):
@@ -16,10 +29,14 @@ def room_view(request, room_name, user_name, role):
     if room_query.exists():
         chat_room = Room.objects.get(name=room_name)
     else:
-        question_url = "https://vktest-kr575za6oa-uw.a.run.app/question"
-        # create error handling for request
-        # question_response = requests.get(question_url).json()['question']
-        question_response = "You wake up with the net worth of Jeff Bezos, what do you do?"
+        try:
+            # Make the request to the API
+            question_url = "https://vktest-kr575za6oa-uw.a.run.app/question"
+            question_response = requests.get(question_url).json()['question']
+
+        except:
+            # Handle the error by selecting a random question from the list
+            question_response = random.choice(questions)
         chat_room = Room.objects.create(name=room_name, question=question_response)
 
     if role % 2 == 0:
@@ -39,8 +56,14 @@ def question_view(request, room_name, user_name, role, old_question):
     room_query = Room.objects.filter(name=room_name, question = old_question)
     if room_query.exists():
         Room.objects.filter(name=room_name, question=old_question).delete()
-        question_url = "https://vktest-kr575za6oa-uw.a.run.app/question"
-        question_response = requests.get(question_url).json()['question']
+        try:
+            # Make the request to the API
+            question_url = "https://vktest-kr575za6oa-uw.a.run.app/question"
+            question_response = requests.get(question_url).json()['question']
+
+        except:
+            # Handle the error by selecting a random question from the list
+            question_response = random.choice(questions)
         chat_room = Room.objects.create(name=room_name, question=question_response)
     else:
         chat_room = Room.objects.get(name=room_name)
